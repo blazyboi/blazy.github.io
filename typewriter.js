88c6508c602ed2272fc42582613c8922d1d7d532
jayviswisely <104494669+jayviswisely@users.noreply.github.com>
2023-04-18 09:52:20 +0700

    Reworking Website

---
+++ typewriter.js
@@ -0,0 +1,41 @@
+const typewriterSpan = document.querySelector(".home-typewrite");
+const cursorSpan = document.querySelector(".home-typewrite-cursor");
+
+const textArray = ["Front-End Developer", "Game Developer", "Guitarist", "Pixel Artist", "Tech Enthusiast", "Programming Tutor", "Freelancer"];
+const typingDelay = 50;
+const erasingDelay = 40;
+const newTextDelay = 1200; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typewriterSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  }
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typewriterSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
